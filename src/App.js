c0039aab0d315a0786d606517687f27ff1ce4b6b
Tyrone Davis III <td3@mit.edu>
2020-10-31 19:25:47 -0400

    Updated README and front text

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mehki's and Ty's workshop
-        </p>
-        - TDIII Group
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Uncommon Questions (Beta)
+                </p>
+                <p>
+                    A neutral and objective source of people's opinions on some of the country's hottest topics.
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
